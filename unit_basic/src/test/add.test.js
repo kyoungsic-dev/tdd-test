7d3bdad7717b36ed031964fca5b097b607050997
kyoungsic <sick93s@naver.com>
2023-09-21 15:45:48 +0900

    Update

---
+++ unit_basic/src/test/add.test.js
@@ -0,0 +1,8 @@
+const add = require('../add.js');
+
+test('3 + 5 = 8', () => {
+  expect(add(3, 5)).toBe(8);
+  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
+});
